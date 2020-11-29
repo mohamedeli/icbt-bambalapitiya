ca4539d9314c34b66445961a53e2093430165731
mohamedeli <73587755+mohamedeli@users.noreply.github.com>
2020-11-29 11:14:59 +0530

    added getter and setter

--- CL-BSCSD-21-13/mavenproject1/src/main/java/package1/Student.java
+++ CL-BSCSD-21-13/mavenproject1/src/main/java/package1/Student.java
@@ -11,6 +11,30 @@ package package1;
  */
 public class Student extends Person {
        private String id;
-       
+        private String firstname;
+    private String lastname;
+    
+    public String getFirstname() {
+        return this.firstname;
+    }
+    
+    public void setFirstname (String firstname){
+        this.firstname = firstname;
+    }
+    public String getLastname (){
+        return lastname;
+    }
+    
+    public void setLastname (String lastname){
+        this.lastname = lastname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
     
 }
