339fd0ae92bbb918c14e46741f22534e3f10f03b
mohamedeli <73587755+mohamedeli@users.noreply.github.com>
2020-11-29 10:07:22 +0530

    testing
    using git desktop

---
+++ CL-BSCSD-21-13/mavenproject1/src/main/java/package1/Person.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package package1;
+
+/**
+ *
+ * @author Mohamed Ali
+ */
+class Person {
+    private String firstname;
+    private String lastname;
+    
+    public String getFirstname() {
+        return this.firstname;
+    }
+    
+    public void setFirstname (String firstname){
+        this.firstname = firstname;
+    }
+    public String getLastname (){
+        return lastname;
+    }
+    
+    public void setLastname (String lastname){
+        this.lastname = lastname;
+    }
+}
---
+++ CL-BSCSD-21-13/mavenproject1/src/main/java/package1/Student.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package package1;
+
+/**
+ *
+ * @author Mohamed Ali
+ */
+public class Student extends Person {
+       private String id;
+       
+    
+}
